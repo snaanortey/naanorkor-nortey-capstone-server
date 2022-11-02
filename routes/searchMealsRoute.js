202efb59930f8ca017af66d286e71af850512719
Naa Norkor <nortey.sarah@gmail.com>
2022-11-02 20:11:34 +0000

    creates get API - takes body params as input, returns array of objects

---
+++ routes/searchMealsRoute.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const router = express.Router();
+
+const elasticClient = require("../elastic-client");
+
+const transformElasticSearchObject = (item) => {
+  const result = {
+    id: item._id,
+    title: item.fields.title[0],
+  };
+  return result;
+};
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await elasticClient.search({
+      index: "recipes",
+      _source: false,
+      query: {
+        match_phrase_prefix: {
+          title: req.query.query,
+        },
+      },
+      fields: ["title"],
+    });
+
+    console.log(result);
+
+    const array = result.hits.hits;
+
+    const apiResponse = array.map(transformElasticSearchObject);
+
+    console.log(apiResponse);
+
+    res.send(apiResponse);
+  } catch (err) {
+    console.log(err);
+    res.status(404).send("failed to load meal suggestions");
+  }
+});
+
+module.exports = router;
